8991775baa52184e291b04ebdfeeaca8f3750518
ahmet.celik <ahmet.celik@tripy.mobi>
2025-09-18 23:18:50 +0300

    redis package added

--- bash_files/redis_installer.sh
+++ bash_files/redis_installer.sh
@@ -12,3 +12,11 @@ if ! command -v redis-server &> /dev/null; then
 else
     echo "✅ Redis already installed."
 fi
+
+# shellcheck disable=SC2034
+CURRENT_PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
+
+sudo apt install -y php${CURRENT_PHP_VERSION}-redis
+
+
+sudo systemctl restart php${CURRENT_PHP_VERSION}-fpm
